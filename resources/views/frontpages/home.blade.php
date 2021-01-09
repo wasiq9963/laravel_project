fca185939f98a326ebf7d9a76b3a3a1d492922ad
wasiq9963 <wasiq5884@gmail.com>
2021-01-09 15:18:04 +0500

    1/9/2021

--- resources/views/frontpages/home.blade.php
+++ resources/views/frontpages/home.blade.php
@@ -17,7 +17,9 @@
 </div>
 <h1 class="text-center">Products</h1>
 
-<div id="card"></div>
+<div class="row" id="card" >
+
+</div>
 @section('javascript')
 @parent
 <script>
@@ -38,27 +40,28 @@
         success:function(response)
         {
           var len = 0;
-          var html;
+          var html = '';
         if(response.result != null)
         {
               len = response.result.length;
         }
         if(len > 0)
         {
-          for( response.result )
+          for( i=0; i<len; i++)
           {
-              console.log(response.result[i].product_name);
-              html = '<div class="card" style="width: 18rem;">';
-              html +='<img class="card-img-top" src={{URL::to('/')}}/images/'+responce.result[i].image+' alt="Card image cap">';
+              //console.log(response.result[i].product_name);
+              html += '<div class="col-md-4">';
+              html += '<div class="card" style="width: 18rem;">';
+              html +='<img class="card-img-top" src={{URL::to('/')}}/images/'+response.result[i].image+' width="100%" heigth="100%" class="img-thumbnail">';
               html +='<div class="card-body">';
-              html += '<h5 class="card-title">'+ responce.result[i].product_name +'</h5>';
-              html +='<p class="card-text">Price:<b>'+ responce.result[i].price +'</b></p>';
+              html += '<h5 class="card-title">'+ response.result[i].product_name +'</h5>';
+              html +='<p class="card-text">Price:<b>'+ response.result[i].price +'</b></p>';
               html +='<a href="#" class="btn btn-dark">Add To Card</a>';
-              html +='</div> </div>';
-              //html = '<h1>'+response.result[i].product_name+'</h1>';
-              $('#card').html(html);
-
+              html +='</div> </div> </div>';
+             // html = '<h1>'+response.result[i].product_name+'</h1>';
+              //alert(response.result[i].product_name);
             }
+            $('#card').html(html);
             
           }
         }
